a462cfefe5f9c4a49e599ccbfcfc91a2c13d7fec
contre88 <mailto:trecon@yandex.ru>
2018-09-07 21:01:37 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,12 +6,12 @@ package ru.job4j.calculate;
  * since 07.09.2018
  * version 1
 */
-public class Calculate{
+public class Calculate {
 	/**
 	 * Main.
 	 * @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
