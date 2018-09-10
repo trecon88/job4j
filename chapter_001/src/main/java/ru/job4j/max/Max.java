3bfe34dd2bae8675b4d6e03560545167d6999614
contre88 <mailto:trecon@yandex.ru>
2018-09-10 13:44:50 +0300

    4.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,17 +1,28 @@
 package ru.job4j.max;
 
 /**
- * Класс для определения наибольшего из двух чисел.
+ * Класс для определения наибольшего из чисел.
  * @author contre
  */
 public class Max {
     /**
-     * Определение наибольшего числа.
+     * Определение наибольшего из двух чисeл.
      * @param first первое число.
      * @param second второе число.
      * @return наибольшее число.
      */
     public int max(int first, int second) {
         return (first > second) ? first : second;
+    }
+
+    /**
+     * Определение наибольшего из трех чисел.
+     * @param first первое число.
+     * @param second второе число.
+     * @param third третье число.
+     * @return наибольшее число.
+     */
+    public int max(int first, int second, int third) {
+        return this.max(this.max(first, second), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -7,11 +7,21 @@ import static org.junit.Assert.*;
 
 public class MaxTest {
     @Test
-    public void whenFirstLessSecontThenSecond() {
+    public void whenFirstLessSecondThenSecond() {
         Max maximum = new Max();
 
         int result = maximum.max(2, 4);
         int expected = 4;
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSecondMaxOfThreeThenSecond() {
+        Max maximum = new Max();
+
+        int result = maximum.max(2, 8, 3);
+        int expected = 8;
 
         assertThat(result, is(expected));
     }
